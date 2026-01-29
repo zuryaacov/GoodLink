fcd132f7fae0b4179c774de7ca3f4b8abb705603
biznes <yaacov@biznes.co.il>
2026-01-29 13:08:42 +0200

    new virsion

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import SocialProof from '../components/SocialProof';
 import Features from '../components/Features';
 import AnalyticsSection from '../components/AnalyticsSection';
 import CTASection from '../components/CTASection';
@@ -25,7 +24,6 @@ const Homepage = () => {
       <Navbar />
       <main className="flex-1">
         <Hero />
-        <SocialProof />
         <Features />
         <AnalyticsSection />
         <CTASection />
