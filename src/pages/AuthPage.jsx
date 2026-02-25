a1f168c1d2f4cb5928acfb87755798ece95d8ca1
biznes <yaacov@biznes.co.il>
2026-02-25 22:05:49 +0200

    new virsion

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -584,7 +584,7 @@ const AuthPage = () => {
           ></path>
           <path
             d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
-            stroke="#0b996f"
+            stroke="#00F59B"
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth="4"
