eb9175e6d2ab8ed4eac3e0b2138f6cba747935b4
biznes <yaacov@biznes.co.il>
2026-02-25 19:13:02 +0200

    new virsion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
             <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
-                stroke="#6358de"
+                stroke="#c0ffa5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="3"
--- src/pages/AbuseReportPage.jsx
+++ src/pages/AbuseReportPage.jsx
@@ -200,14 +200,14 @@ const AbuseReportPage = () => {
               <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
-                  stroke="#135bec"
+                  stroke="#c0ffa5"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="3"
                 />
                 <path
                   d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
-                  stroke="#10b981"
+                  stroke="#0b996f"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="3"
