c740d197bd439ec24fc163faa98ae45e67c5d3f0
biznes <yaacov@biznes.co.il>
2026-02-25 22:10:08 +0200

    new virsion

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -27,7 +27,7 @@ const DashboardLayout = () => {
               <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
-                  stroke="#c0ffa5"
+                  stroke="#00F59B"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="3"
