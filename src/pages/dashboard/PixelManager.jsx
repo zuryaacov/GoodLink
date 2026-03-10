cd2326652a30a83cda8bd740ddb5258d8f67ad5b
biznes <yaacov@biznes.co.il>
2026-03-10 20:10:31 +0200

    new virsion

--- src/pages/dashboard/PixelManager.jsx
+++ src/pages/dashboard/PixelManager.jsx
@@ -266,33 +266,33 @@ const PixelManager = () => {
             </svg>
           </div>
         );
-      case 'snapchat':
-        // Snapchat logo from simpleicons.org
-        return (
-          <div className="w-10 h-10 rounded-lg bg-[#FFFC00] flex items-center justify-center">
-            <svg
-              viewBox="0 0 24 24"
-              width="20"
-              height="20"
-              fill="black"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.174-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.097.118.112.222.083.343-.09.375-.293 1.199-.334 1.363-.053.225-.172.271-.402.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.357-.629-2.758-1.379l-.749 2.848c-.269 1.045-1.004 2.352-1.498 3.146 1.123.345 2.306.535 3.487.535 6.624 0 11.99-5.367 11.99-11.987C23.97 5.39 18.592.026 11.976.026L12.017 0z" />
-            </svg>
-          </div>
-        );
-      case 'taboola':
-        return (
-          <div className="w-10 h-10 rounded-lg flex items-center justify-center overflow-hidden border border-slate-700">
-            <img src={taboolaLogo} alt="Taboola" className="w-full h-full object-cover" />
-          </div>
-        );
-      case 'outbrain':
-        return (
-          <div className="w-10 h-10 rounded-lg flex items-center justify-center overflow-hidden border border-slate-700">
-            <img src={outbrainLogo} alt="Outbrain" className="w-full h-full object-cover" />
-          </div>
-        );
+      // case 'snapchat':
+      //   // Snapchat logo from simpleicons.org
+      //   return (
+      //     <div className="w-10 h-10 rounded-lg bg-[#FFFC00] flex items-center justify-center">
+      //       <svg
+      //         viewBox="0 0 24 24"
+      //         width="20"
+      //         height="20"
+      //         fill="black"
+      //         xmlns="http://www.w3.org/2000/svg"
+      //       >
+      //         <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.174-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.097.118.112.222.083.343-.09.375-.293 1.199-.334 1.363-.053.225-.172.271-.402.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.357-.629-2.758-1.379l-.749 2.848c-.269 1.045-1.004 2.352-1.498 3.146 1.123.345 2.306.535 3.487.535 6.624 0 11.99-5.367 11.99-11.987C23.97 5.39 18.592.026 11.976.026L12.017 0z" />
+      //       </svg>
+      //     </div>
+      //   );
+      // case 'taboola':
+      //   return (
+      //     <div className="w-10 h-10 rounded-lg flex items-center justify-center overflow-hidden border border-slate-700">
+      //       <img src={taboolaLogo} alt="Taboola" className="w-full h-full object-cover" />
+      //     </div>
+      //   );
+      // case 'outbrain':
+      //   return (
+      //     <div className="w-10 h-10 rounded-lg flex items-center justify-center overflow-hidden border border-slate-700">
+      //       <img src={outbrainLogo} alt="Outbrain" className="w-full h-full object-cover" />
+      //     </div>
+      //   );
       default:
         return (
           <div className="w-10 h-10 rounded-lg bg-slate-200 flex items-center justify-center">
@@ -312,12 +312,12 @@ const PixelManager = () => {
         return 'TikTok';
       case 'google':
         return 'Google Ads';
-      case 'snapchat':
-        return 'Snapchat';
-      case 'taboola':
-        return 'Taboola';
-      case 'outbrain':
-        return 'Outbrain';
+      // case 'snapchat':
+      //   return 'Snapchat';
+      // case 'taboola':
+      //   return 'Taboola';
+      // case 'outbrain':
+      //   return 'Outbrain';
       default:
         return platform;
     }
